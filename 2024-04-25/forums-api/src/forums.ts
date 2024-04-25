fa05b95b23b6c63a47ca11cc3dbd988133b791b6
Pau Fernández <pau.fernandez@upc.edu>
2024-04-25 20:54:09 +0200

    Extraigo el endpoint de /forums a forums.ts (versión DIY)

---
+++ 2024-04-25/forums-api/src/forums.ts
@@ -0,0 +1,23 @@
+import type { Express } from "express";
+import { db } from "./db";
+
+/*
+GET  /forums
+POST /forums
+GET  /forums/:id
+PUT  /forums/:id
+DELETE /forums/:id
+*/
+
+export const setupForumEndpoints = (app: Express) => {
+  app.get("/forums", async (req, res) => {
+    try {
+      const forums = await db.forum.findMany({
+        orderBy: { createdAt: "asc" },
+      });
+      res.status(200).json(forums);
+    } catch (e) {
+      res.status(500).json({ error: "Internal Error" });
+    }
+  });
+};
--- 2024-04-25/forums-api/src/server.ts
+++ 2024-04-25/forums-api/src/server.ts
@@ -1,16 +1,22 @@
 import express from "express";
 import morgan from "morgan";
 import cors from "cors";
+import { db } from "./db";
+import { setupForumEndpoints } from "./forums";
 
 const app = express();
+
+/* 
+GOLDEN RULE of express SERVERS: 
+Si tus handlers son async, tiene que poner un try-catch por fuerza!!
+Si no, tu servidor se puede morir
+*/
 
 app.use(cors());
 app.use(morgan("dev"));
 app.use(express.json());
 
-app.get("/", async (req, res) => {
-  res.status(200).json({ ok: true, message: "Hello!" });
-});
+setupForumEndpoints(app);
 
 const { PORT } = process.env;
 app.listen(PORT, () => {
