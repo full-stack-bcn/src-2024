0a0ca45d89670f5553c3d90932f6cbc4f022bd90
Pau Fernández <pau.fernandez@upc.edu>
2024-04-25 21:33:06 +0200

    Mayoría de endpoints para /forums

--- 2024-04-25/forums-api/src/forums.ts
+++ 2024-04-25/forums-api/src/forums.ts
@@ -18,7 +18,7 @@ router.get("/", async (req, res) => {
       select: {
         name: true,
         forumId: true,
-      }
+      },
     });
     res.status(200).json({ forums });
   } catch (e) {
@@ -26,4 +26,36 @@ router.get("/", async (req, res) => {
   }
 });
 
-export default router
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    const forum = await db.forum.findUniqueOrThrow({
+      where: { forumId: Number(id) },
+    });
+    res.status(200).json({ forum });
+  } catch (e: any) {
+    if (e.name === "NotFoundError") {
+        return res.status(404).json({ message: `Not found.` });
+    }
+    res.status(500).json({ error: `Internal error` });
+  }
+});
+
+router.post("/", async (req, res) => {
+  try {
+    const { name } = req.body;
+    // Chequeo de los datos de entrada
+    if (name === undefined || typeof name !== "string") {
+      return res.status(400).json({ error: "Missing `name` field" });
+    }
+    const forum = await db.forum.create({
+      data: { name },
+    });
+    res.status(201).json(forum);
+  } catch (e) {
+    console.error(e);
+    res.status(500).json({ error: "Couldn't create forum. Come back later... ;)" });
+  }
+});
+
+export default router;
