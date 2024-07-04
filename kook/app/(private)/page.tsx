c98eca1ae6368ca58f5e53cbb3e1308c5c856656
Pau Fernández <pau.fernandez@upc.edu>
2024-07-04 19:03:12 +0200

    Añado Card para nueva receta

--- kook/app/(private)/page.tsx
+++ kook/app/(private)/page.tsx
@@ -1,6 +1,8 @@
 import RecipeCard from "@/components/RecipeCard";
 import { dbGetUserRecipes } from "@/db/recipes";
 import { serverGetUser } from "@/db/users";
+import Link from "next/link";
+import { PlusCircle } from 'lucide-react';
 
 export default async function Home() {
   const user = await serverGetUser();
@@ -12,6 +14,12 @@ export default async function Home() {
         {userRecipes.map((ur) => (
           <RecipeCard key={ur.id} recipe={ur} />
         ))}
+        <Link href="/recipe/new">
+          <div className="w-32 h-48 bg-gray-100 rounded p-2 px-3 flex flex-col justify-center items-center">
+            <PlusCircle className="opacity-60" />
+            <div className="opacity-60 text-sm">New Recipe</div>
+          </div>
+        </Link>
       </div>
     </main>
   );
